a2ca175f1c3c25a1216ce62fe91dffe439d01aca
Imam Sutono <imamsutono>
2023-08-16 10:13:57 +0700

    tidy up print result

--- descriptive_analysis.py
+++ descriptive_analysis.py
@@ -6,9 +6,11 @@ data = json.load(jsonfile)
 
 df = pd.DataFrame(data, columns=['age', 'sex', 'default'])
 
+print("--Descriptive Analysis--\n")
+
 # Umur
 mean_age = int(df['age'].mean())
-print("Rata-rata umur pelanggan: " + str(mean_age) + " tahun")
+print(f"Rata-rata umur pelanggan: {str(mean_age)} tahun\n")
 
 # Jenis kelamin
 customer_by_sex = df['sex'].value_counts(normalize=True) * 100
@@ -20,6 +22,6 @@ for index, value in customer_by_sex.items():
 # Default
 default_credit = df[df['default'] == 1]
 default_credit_percent = (len(default_credit) / len(df['default'])) * 100
-print("Proporsi pelanggan yang mengalami default: " + str(default_credit_percent) + "%")
+print(f"\nProporsi pelanggan yang mengalami default: {str(default_credit_percent)}%")
 
 jsonfile.close()
