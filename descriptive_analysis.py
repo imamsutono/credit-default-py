08c05dddf9962ece776f9cf65b31f9439b550ed2
Imam Sutono <imamsutono>
2023-08-16 10:02:18 +0700

    add mean age analysis

--- descriptive_analysis.py
+++ descriptive_analysis.py
@@ -6,6 +6,9 @@ data = json.load(jsonfile)
 
 df = pd.DataFrame(data, columns=['age', 'default'])
 
+mean_age = int(df['age'].mean())
+print("Rata-rata umur pelanggan: " + str(mean_age) + " tahun")
+
 default_credit = df[df['default'] == 1]
 default_credit_percent = (len(default_credit) / len(df['default'])) * 100
 print("Proporsi pelanggan yang mengalami default: " + str(default_credit_percent) + "%")
